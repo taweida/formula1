afe34141fd1f46698e574b5b5621885ead37e753
taweida <trevor.aweida@colorado.edu>
2022-03-21 15:02:34 -0600

    testing php instead

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title> Aweida Formula 1 </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="favicon.ico">
+</head>
+
+<body>
+    <section class="bg"></section>
+    <div class="loading-text">0%</div>
+    <script src="script.js"></script>
+</body>
