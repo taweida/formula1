5c822e256c494714a0bcf56891fd86e8f12144e6
taweida <trevor.aweida@colorado.edu>
2022-03-21 16:52:28 -0600

    First rough draft

--- index.php
+++ index.php
@@ -7,31 +7,24 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="icon" href="favicon.ico">
 </head>
+<body>
+    <section class="myHead">
+        <h1> Welcome to Aweida Formula 1 - <strong>2022</strong></h1>
+    </section>
+    <div class="bodyForLoading">
+        <section class="bg"></section>
+        <div class="loading-text">0%</div>
+        <div class="container">
+            <div class="item">
 
-<nav class="navbar navbar-expand-sm bg-secondary navbar-dark"
-<ul class="navbar-nav">
-    <li class="nav-item">
-        <a class="navbar-brand" href="#">Home</a>
-    </li>
-    <li class="nav-item">
-        <a class="navbar-brand" href="#">Projects</a>
-    </li>
-    <li class="nav-item">
-        <a class="navbar-brand" href="#">Sign up</a>
-    </li>
-    <li class="nav-item">
-        <a class="navbar-brand" href="#">Tic-Tac-Toe</a>
-    </li>
-    <ul class="nav navbar-nav ml-auto">
-        <li>
-            <a href="https://github.com/CSCI-3308-CU-Boulder/TrevorAweida_section013_Labs"> 
-                <img src="https://image.flaticon.com/icons/png/512/25/25231.png" width="40" height="40" alt="">
-            </a>
-        </li>
-    </ul>
-</ul>
-</nav>
-<body class="bodyForLoading">
-    <div class="loading-text">0%</div>
-    <script src="script.js"></script>
+            </div>
+            <div class="item">
+
+            </div>
+            <div class="item">
+                
+            </div>
+        </div>
+        <script src="script.js"></script>
+    </div>
 </body>
--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+// Tutorial for fancy shit lol : https://www.youtube.com/watch?v=JkeyKeK3V24
+
 const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
+const head = document.querySelector('.myHead')
+const container = document.querySelector('.container')
 
-let load = 0
+let loadFirst = 0
 
-let int = setInterval(blurring, 30)
+let intFirst = setInterval(blurring, 15)
 
 function blurring(){
-    load++
-    if(load > 99){
-        clearInterval(int)
+    loadFirst++
+    if(loadFirst > 99){
+        clearInterval(intFirst)
     }
-    loadText.innerText = `${load}%`
-    loadText.style.opacity = scale(load, 0, 100, 1, 0)
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+    loadText.innerText = `${loadFirst}%`
+    loadText.style.opacity = scale(loadFirst, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(loadFirst, 0, 100, 30, 0)}px)`
+    head.style.opacity = scale(loadFirst, 0, 100, 0, 1)
+    container.style.opacity = scale(loadFirst, 0, 100, 0, 1)
 }
 
 function scale (number, inMin, inMax, outMin, outMax) {
@@ -20,3 +26,4 @@ function scale (number, inMin, inMax, outMin, outMax) {
 }
 
 // From : https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+
