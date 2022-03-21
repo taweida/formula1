1cd77e17de086eb4426258e176ee412615ed8928
taweida <trevor.aweida@colorado.edu>
2022-03-21 16:18:36 -0600

    Testing formatting

--- index.php
+++ index.php
@@ -4,18 +4,34 @@
     <meta charset="utf-8">
     <title> Aweida Formula 1 </title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="icon" href="favicon.ico">
 </head>
 
+<nav class="navbar navbar-expand-sm bg-secondary navbar-dark"
+<ul class="navbar-nav">
+    <li class="nav-item">
+        <a class="navbar-brand" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+        <a class="navbar-brand" href="#">Projects</a>
+    </li>
+    <li class="nav-item">
+        <a class="navbar-brand" href="#">Sign up</a>
+    </li>
+    <li class="nav-item">
+        <a class="navbar-brand" href="#">Tic-Tac-Toe</a>
+    </li>
+    <ul class="nav navbar-nav ml-auto">
+        <li>
+            <a href="https://github.com/CSCI-3308-CU-Boulder/TrevorAweida_section013_Labs"> 
+                <img src="https://image.flaticon.com/icons/png/512/25/25231.png" width="40" height="40" alt="">
+            </a>
+        </li>
+    </ul>
+</ul>
+</nav>
 <body>
-    
-    <section class="bg"></section>
     <div class="loading-text">0%</div>
     <script src="script.js"></script>
-    <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">News</a></li>
-        <li><a href="#">Contact</a></li>
-        <li><a href="#">About</a></li>
-    </ul>
 </body>
